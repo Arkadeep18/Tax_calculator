d1964203b93d7a902911ae66a19d5760f67e99e7
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-24 14:36:50 +0530

    Update Tax_Calculator.java

--- Tax_Calculator.java
+++ Tax_Calculator.java
@@ -8,19 +8,17 @@ public class Tax_Calculator {
 		double income = sc.nextDouble();
 		
 		double tax = 0;
-		
-		if(income <= 2.5) {
+		//income in lakhs
+		if(income <= 2.5) {		//no tax below 2.5 lakhs
 			tax = 0;
 		}
-		else if(income>2.5 && income<=5.0) {
+		else if(income>2.5 && income<=5.0) {	//5% tax on 2.5 - 5 lakh
 			tax =0.05*(income-2.5);
 		}
-		else if(income>5.0 && income<=10.0) {
+		else if(income>5.0 && income<=10.0) {			//5% tax on 2.5 - 5 lakh & 20% tax on 5 - 10 lakh
 			tax = 0.05*(5.0 - 2.5)+ 0.2*(income-5.0);
 		}
-		else if(income>10.0) {
-			
-			
+		else if(income>10.0) {						//5% tax on 2.5 - 5 lakh & 20% tax on 5 - 10 lakh & 30% tax on 10/upto 10 lakhs
 			tax = 0.05*(5.0 - 2.5) +0.2 *(10.0 - 5.0)+ 0.3*(income-10.0);
 		}
 		System.out.println("Tax: "+tax);
